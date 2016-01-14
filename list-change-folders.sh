8ccbd0ff2505b67f15c896143339d206637fa7aa
ssr42 <ssr@axiell.com>
2016-01-14 16:47:51 +0100

    Update list-change-folders.sh

--- list-change-folders.sh
+++ list-change-folders.sh
@@ -1,3 +1,1 @@
-echo GIT_COMMIT=$GIT_COMMIT
-echo Changed integrations:
-git show --pretty="format:" --name-only $GIT_COMMIT | perl -ne 'while(/integration\/(.*)\//g){print "$1\n";}'
+echo INTEGRATION=$(git show --pretty="format:" --name-only $GIT_COMMIT | perl -ne 'while(/integration\/(.*)\//g){print "$1\n";}') > env.properties
